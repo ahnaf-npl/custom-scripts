f8511b491ae0868313bcfea0bab4345c4de0df41
ahnaf-npl <anhaf@npl.asia>
2025-08-18 13:37:29 +0800

    docs: update kviewer-notranslate.js from web editor

---
+++ kviewer-notranslate.js
@@ -0,0 +1,10 @@
+(function() {
+  'use strict';
+
+  // Menambahkan class "notranslate" ke elemen <html>
+  // document.documentElement merujuk pada tag <html>
+  document.documentElement.classList.add('notranslate');
+  
+  console.log('Class "notranslate" berhasil ditambahkan untuk mencegah auto-translate.');
+
+})();
